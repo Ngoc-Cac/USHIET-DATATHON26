8d4e241171aeaa44b557ed0d2c46ced0764aa126
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-04-21 09:17:41 +0700

    Update data loaders to load only the needed tables
    - Add an option to include only the specified files to load, reducing
      the memory usage.
    
    - Move to using `pathlib` for globbing the files. This is nicer because
      we can directly get the filename without doing any splitting and etc.

--- src/readers.py
+++ src/readers.py
@@ -1,11 +1,34 @@
-import os.path as osp, glob
 import pandas as pd
 
-_EXCLUDE = ["sample_submission.csv"]
+from collections import defaultdict
+from pathlib import Path
 
-def load_csv(data_dir):
+_EXCLUDE = ["sample_submission"]
+
+
+def _is_excluded(filename: str, include_files: list[str] | None = None) -> bool:
+    # some utility check
+    not_included = include_files is not None and filename not in include_files
+    excluded = filename in _EXCLUDE
+    return not_included or excluded
+
+def load_csv(
+    data_dir: str,
+    load_only: list[str] | None = None,
+) -> dict[str, pd.DataFrame]:
+    """
+    Bulk load competition data from `data_dir` directory.
+
+    :param str data_dir: The data directory containing the competition tables.
+    :param list[str] or None load_only: A list of table names to load. When
+        given, this will only load the specified tables, to save memory. If `None`,
+        all tables will be loaded.
+
+    :return: A dictionary with `(table_name, df)` as key-value pairs.
+    :rtype: dict[str, pd.DataFrame]
+    """
     return {
-        osp.split(csv)[-1][:-4]: pd.read_csv(csv)
-        for csv in glob.glob(f"{data_dir}/*.csv")
-        if osp.split(csv)[-1] not in _EXCLUDE
+        csv.stem: pd.read_csv(csv)
+        for csv in Path(data_dir).glob("*.csv")
+        if not _is_excluded(csv.stem, load_only)
     }
